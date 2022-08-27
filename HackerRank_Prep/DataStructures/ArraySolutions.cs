9e580ec3b87c7a802a9f37eb1f27184913b94345
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-08-27 02:24:10 +0100

    Add project files.

---
+++ HackerRank_Prep/DataStructures/ArraySolutions.cs
@@ -0,0 +1,17 @@
+﻿namespace HackerRank_Prep.DataStructures
+{
+    public class ArraySolutions
+    {
+        public static List<int> ReverseArray(List<int> a)
+        {
+            int j = 0;
+            List<int> res = new();
+
+            for (int i = a.Count - 1; i >= 0; i--, j++)
+            {
+                res.Add(a[i]);
+            }
+            return res;
+        }
+    }
+}
---
+++ HackerRank_Prep/Program.cs
@@ -0,0 +1,10 @@
+﻿using HackerRank_Prep.DataStructures;
+
+List<int> list = new() { 2, 3, 5};
+
+var result = ArraySolutions.ReverseArray(list);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
